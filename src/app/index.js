fb13a60fffef3fe998bffc0b149d83b9a94f5076
CC <ccfullstackautomation@gmail.com>
2025-07-15 20:31:04 -0400

    Initial commit

---
+++ src/app/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Whitehall Terrace</h1>
+      <p>Your future of full-stack automation starts here.</p>
+    </div>
+  );
+}
